ad0450feb0bcf1d56589be3dfcb745072c1c91a2
AlessioBona <alefbona@gmail.com>
2018-11-21 13:33:57 +0100

    BackGround Clickable and Text

---
+++ Assets/Scripts/ClickManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ClickManager : MonoBehaviour {
+
+    public TextMeshProUGUI weckerName;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ToggleName()
+    {
+        weckerName.enabled = !weckerName.enabled;
+    }
+
+}
